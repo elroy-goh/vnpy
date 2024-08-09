b7efd552c0969319a7a04ac7cf6fe230c9a0e332
Elroy <gjgoh@hotmail.com>
2024-08-09 12:51:16 +0000

    Changed settings from official release

--- vnpy/trader/setting.py
+++ vnpy/trader/setting.py
@@ -2,7 +2,7 @@
 Global setting of the trading platform.
 """
 
-from logging import CRITICAL
+from logging import CRITICAL, DEBUG
 from typing import Dict, Any
 from tzlocal import get_localzone_name
 
@@ -14,7 +14,7 @@ SETTINGS: Dict[str, Any] = {
     "font.size": 12,
 
     "log.active": True,
-    "log.level": CRITICAL,
+    "log.level": DEBUG,
     "log.console": True,
     "log.file": True,
 
@@ -30,10 +30,10 @@ SETTINGS: Dict[str, Any] = {
     "datafeed.password": "",
 
     "database.timezone": get_localzone_name(),
-    "database.name": "sqlite",
-    "database.database": "database.db",
-    "database.host": "",
-    "database.port": 0,
+    "database.name": "mongodb",
+    "database.database": "vnpy",
+    "database.host": "localhost",
+    "database.port": 27017,
     "database.user": "",
     "database.password": ""
 }
